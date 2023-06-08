fd1cb9281b113fb2677c3891580712127a9e61a0
Matt McElwee <matt.mcelwee@jointhebibleproject.com>
2023-06-08 11:20:44 -0500

    feat: added better sorting

--- src/admin/index.ts
+++ src/admin/index.ts
@@ -1,4 +1,5 @@
 import './styles.css';
+import sortOn from 'sort-on';
 
 const stateRow = (id: number) => `
     <div class="row">
@@ -66,54 +67,51 @@ const main = async (token: string) => {
   const cb = listener(token);
   window.removeEventListener('change', cb);
 
-  table
-    .sort((a, b) => a.role?.turnOrder - b.role?.turnOrder)
-    .forEach((r) => {
-      const row = document.createElement('tr');
-      const turnOrder = document.createElement('td');
-      turnOrder.innerText = r.role?.turnOrder || '';
-      row.appendChild(turnOrder);
+  sortOn(table, ['-killed', 'role.turnOrder', 'role.title']).forEach((r) => {
+    const row = document.createElement('tr');
+    const turnOrder = document.createElement('td');
+    turnOrder.innerText = r.role?.turnOrder || '';
+    row.appendChild(turnOrder);
 
-      const name = document.createElement('td');
-      name.innerText = r.name;
-      row.appendChild(name);
+    const name = document.createElement('td');
+    name.innerText = r.name;
+    row.appendChild(name);
 
-      const role = document.createElement('td');
-      role.innerHTML = `
+    const role = document.createElement('td');
+    role.innerHTML = `
         <span><strong>${r.role?.title}</strong></span>
         <input type="checkbox" aria-label="Role Info" class="info" id="info-${r.id}" />
         <ul>${r.role?.rules
           .map((rule) => `<li>${rule.replaceAll(/\[\[(.*?)]]/g, '$1')}</li>`)
           .join('')}</ul>
       `;
-      row.appendChild(role);
-      (row.querySelector('.info') as HTMLInputElement).checked = !!window.localStorage.getItem(
-        `info-${r.id}`,
-      );
+    row.appendChild(role);
+    (row.querySelector('.info') as HTMLInputElement).checked = !!window.localStorage.getItem(
+      `info-${r.id}`,
+    );
 
-      const state = document.createElement('td');
-      state.innerHTML = stateRow(r.id);
-      row.appendChild(state);
-      (row.querySelector('.killed') as HTMLInputElement).checked = r.killed;
-      (row.querySelector('.bitten') as HTMLInputElement).checked = r.bitten;
-      (row.querySelector('.cursed') as HTMLInputElement).checked = r.cursed;
-      (row.querySelector('.recruited') as HTMLInputElement).checked = r.recruited;
-      (row.querySelector('.protected__vampire') as HTMLInputElement).checked = r.protected_vampire;
-      (row.querySelector('.protected__werewolf') as HTMLInputElement).checked =
-        r.protected_werewolf;
+    const state = document.createElement('td');
+    state.innerHTML = stateRow(r.id);
+    row.appendChild(state);
+    (row.querySelector('.killed') as HTMLInputElement).checked = r.killed;
+    (row.querySelector('.bitten') as HTMLInputElement).checked = r.bitten;
+    (row.querySelector('.cursed') as HTMLInputElement).checked = r.cursed;
+    (row.querySelector('.recruited') as HTMLInputElement).checked = r.recruited;
+    (row.querySelector('.protected__vampire') as HTMLInputElement).checked = r.protected_vampire;
+    (row.querySelector('.protected__werewolf') as HTMLInputElement).checked = r.protected_werewolf;
 
-      if (r.protected_vampire) {
-        (row.querySelector('.recruited') as HTMLInputElement).disabled = true;
-        (row.querySelector('.bitten') as HTMLInputElement).disabled = true;
-      }
+    if (r.protected_vampire) {
+      (row.querySelector('.recruited') as HTMLInputElement).disabled = true;
+      (row.querySelector('.bitten') as HTMLInputElement).disabled = true;
+    }
 
-      if (r.protected_werewolf) {
-        (row.querySelector('.killed') as HTMLInputElement).disabled = true;
-        (row.querySelector('.recruited') as HTMLInputElement).disabled = true;
-      }
+    if (r.protected_werewolf) {
+      (row.querySelector('.killed') as HTMLInputElement).disabled = true;
+      (row.querySelector('.recruited') as HTMLInputElement).disabled = true;
+    }
 
-      document.querySelector('tbody').appendChild(row);
-    });
+    document.querySelector('tbody').appendChild(row);
+  });
   window.addEventListener('change', cb);
   setTimeout(() => main(token), 10 * 1000);
 };

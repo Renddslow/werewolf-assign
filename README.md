c223e672dac9853e2125b7f577fb2d5d0ecf7501
Matt McElwee <matt.mcelwee@jointhebibleproject.com>
2023-06-07 22:11:46 -0500

    feat: added title animation

---
+++ README.md
@@ -0,0 +1,11 @@
+# Werewolf by Zoom
+
+> A game of deception, deduction, and werewolves, designed for remote play.
+
+## Getting Started
+
+```shell
+$ git clone git@github.com:Renddslow/werewolf-assign
+$ yarn && yarn build
+$ yarn start
+```
--- src/client/index.ts
+++ src/client/index.ts
@@ -6,4 +6,11 @@ import RoleCard from './RoleCard';
   if (!window.customElements.get('role-card')) {
     customElements.define('role-card', RoleCard);
   }
+
+  const userHash = window.localStorage.getItem('werewolf:user');
+  if (!userHash) {
+    // Render login page
+  } else {
+    // Render admin page
+  }
 })();
